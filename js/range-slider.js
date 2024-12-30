3bee5f7b7a9f1b6ace46f633a7d8342929261835
Alina Vagner <ali.vag977@gmail.com>
2024-12-30 16:54:24 +0300

    fixes: change focus summary
    change https://null-smth.com/ to #
    
    fixes:https://null-smth-post.com/ to #
    
    delete <hr/> and fix blog-details style
    
    add heading for asset catalog
    
    fix main cta button
    
    add like style for like button
    
    fix units
    
    delete scrollbar-gutter
    
    add autoprefixer and purgeCSS and set them
    
    connect purgeCSS with slider and select
    
    add scripts
    
    add cursor for details and slider
    
    add stylelint
    
    minify content

--- js/range-slider.js
+++ js/range-slider.js
@@ -1,8 +1,1 @@
-$(".js-range-slider").ionRangeSlider({
-  type: "double",
-  min: 5,
-  max: 50,
-  from: 15,
-  to: 37,
-  prefix: "€",
-});
+$(".js-range-slider").ionRangeSlider({type:"double",min:5,max:50,from:15,to:37,prefix:"€",});
